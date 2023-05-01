291e8398f170680202eeaed4bd10ebf4267e7d13
Gustavo-SLemos <gustavo.lemossc@gmail.com>
2023-05-01 11:31:33 -0300

    projeto em React atualizado

---
+++ REACT/organo/src/componentes/Banner/index.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+
+function Banner() {
+  //JSX
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.png"
+        alt="imagem do banner principal na cor azul"
+      />
+    </header>
+  );
+}
+
+export default Banner;
